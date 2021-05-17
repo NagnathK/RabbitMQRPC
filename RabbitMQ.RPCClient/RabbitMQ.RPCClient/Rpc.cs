08487387bba5daa093bd4ec48a99f5c1467fbced
Nagnath Katkar <nagnathkatkar@gmail.com>
2021-05-17 22:43:58 +0530

    Initial commit

---
+++ RabbitMQ.RPCClient/RabbitMQ.RPCClient/Rpc.cs
@@ -0,0 +1,37 @@
+﻿namespace RabbitMQ.RPCClient
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class Rpc
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("RPC Client");
+
+            string n = args.Length > 0 ? args[0] : "30";
+
+            Task t = InvokeAsync(n);
+
+            t.Wait();
+
+            Console.WriteLine(" Press [enter] to exit.");
+
+            Console.ReadLine();
+        }
+
+        private static async Task InvokeAsync(string n)
+        {
+            var rpcClient = new RpcClient();
+
+            Console.WriteLine(" [x] Requesting fib({0})", n);
+
+            var response = await rpcClient.CallAsync(n.ToString());
+
+            Console.WriteLine(" [.] Got '{0}'", response);
+
+            rpcClient.Close();
+        }
+
+    }
+}
